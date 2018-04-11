7580b06913a53d1c54e76209376ebc3c6276f79d
claireb25 <claire.bourgeois.cb@gmail.com>
2018-04-11 13:51:44 +0200

    x

--- traitement.php
+++ traitement.php
@@ -1,17 +1,14 @@
 <?php
-$content = ["listDir"=> "", "listFile"=> ""]; // on créé un tableau qui contient deux éléments "listDir" et "listFile" qui sont vides
-$content['listDir'] = array_filter(glob("*"), "is_dir"); // on remplit listDir - on applique un filtre "dossier" --> is_dir, sur notre dossier glob("*")
-$content['listFile'] = array_filter(glob("./*"), "is_file"); // on remplit listFile - on applique un filtre "fichiers" --> is_file, sur notre dossier glob("*")
-// glob("*") = je parcours les éléments 
-var_dump($content);
 
-
-
+$path = "./*";
 
 
 if (isset($_POST['path'])){
     $path = $_POST['path'];
-    
+    $content = ["listDir"=> "", "listFile"=> ""]; // on créé un tableau qui contient deux éléments "listDir" et "listFile" qui sont vides
+    $content['listDir'] = array_filter(glob($path), "is_dir"); // on remplit listDir - on applique une fonction callback -> is_dir, qui va tester l'url, sur notre dossier glob("*")
+    $content['listFile'] = array_filter(glob($path), "is_file"); // on remplit listFile - on applique un filtre "fichiers" --> is_file, sur notre dossier glob("*")
+    // glob("*") = je parcours les éléments 
 
 }
 
