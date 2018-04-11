5819b2cc9beb1795721a2b3945711919bc2a09b5
claireb25 <claire.bourgeois.cb@gmail.com>
2018-04-11 16:01:40 +0200

    différence fichiers/dossiers et dossiers cliquables

--- ajax.js
+++ ajax.js
@@ -1,3 +1,5 @@
+// Déclaration de la méthode et des paramètres Ajax
+
 var data = new FormData();
     data.append("path","./*");
 
@@ -6,7 +8,7 @@ var paramAjax = {
     body : data
 };
 
-
+// fetch et affichage des élémlents du fichier PHP
 fetch("traitement.php", paramAjax).then(function(response) {
   return response.json();
 })
@@ -16,7 +18,15 @@ fetch("traitement.php", paramAjax).then(function(response) {
         console.log("response[elements] = "+elements);
         for (donnees in response[elements]){
             console.log(" valeur des éléments de la réponse = "+response[elements][donnees]);
-            document.getElementById('injectionAjax').innerHTML += response[elements][donnees] + "<br>";
+            var url = response[elements][donnees].slice(2); // permet de retirer le "./" -> affiche à partir du 2ème élement de la chaine de caractères
+            if (elements == "listDir"){
+                document.getElementById('injectionAjax').innerHTML += '<a href="' +response[elements][donnees] +'">' +url + '<br>';
+            }
+            else if (elements == "listFile"){
+                document.getElementById('injectionAjax').innerHTML += url + "<br>";
+            }
         }
     }
 });
+
+// Quand on clique sur un dossier ... 
