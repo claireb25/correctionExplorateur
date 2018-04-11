cca3f52c79ea5b1939fffbe0006b38456afa6a87
claireb25 <claire.bourgeois.cb@gmail.com>
2018-04-11 12:02:23 +0200

    + commentaires glob

--- traitement.php
+++ traitement.php
@@ -1,8 +1,8 @@
 <?php
 $content = ["listDir"=> "", "listFile"=> ""]; // on créé un tableau qui contient deux éléments "listDir" et "listFile" qui sont vides
 $content['listDir'] = array_filter(glob("*"), "is_dir"); // on remplit listDir - on applique un filtre "dossier" --> is_dir, sur notre dossier glob("*")
-$content['listFile'] = array_filter(glob("*"), "is_file"); // on remplit listDir - on applique un filtre "fichiers" --> is_file, sur notre dossier glob("*")
-
+$content['listFile'] = array_filter(glob("./*"), "is_file"); // on remplit listFile - on applique un filtre "fichiers" --> is_file, sur notre dossier glob("*")
+// glob("*") = je parcours les éléments 
 var_dump($content);
 
 
